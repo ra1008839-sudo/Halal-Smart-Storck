7c2f9f01a681116609b6062698ab34654214b475
ra1008839-sudo <ra1008839@gmail.com>
2025-08-07 00:06:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(_name_)
+
+# Simple GPT Power logic (placeholder)
+def gpt_power_signal(volume):
+    if volume > 900000:
+        return "BUY", "green"
+    elif volume > 500000:
+        return "WAIT", "orange"
+    else:
+        return "AVOID", "red"
+
+@app.route('/get-signal', methods=['POST'])
+def get_signal():
+    data = request.get_json()
+    volume = data.get("volume", 0)
+    stock = data.get("stock", "Unknown")
+
+    signal, color = gpt_power_signal(volume)
+
+    response = {
+        "stock": stock,
+        "volume": volume,
+        "signal": signal,
+        "color": color,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return jsonify(response)
+
+@app.route('/')
+def home():
+    return "Halal Smart Stock App is running!"
+
+if _name_ == '_main_':
+    app.run(debug=True)
