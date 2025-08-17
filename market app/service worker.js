5293da1e8490c47bde099a9beafc9411050dbb96
ra1008839-sudo <ra1008839@gmail.com>
2025-08-18 00:59:37 +0530

    Create service worker.js

---
+++ market app/service worker.js
@@ -0,0 +1,26 @@
+const CACHE = "hss-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
