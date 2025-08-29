3cbff58d06966cc18e344efb2bafa9c62bf6e753
ra1008839-sudo <ra1008839@gmail.com>
2025-08-30 00:59:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return "<h1>Halal Smart Stock is Live 🚀</h1><p>App deployed successfully!</p>"
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(host="0.0.0.0", port=5000)
